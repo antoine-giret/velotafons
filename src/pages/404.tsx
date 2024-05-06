d175f6ffcc65a254ce02877bf70cc27cd2eeef85
Antoine Giret <antoine.giret@geovelo.fr>
2024-05-06 10:07:14 +0200

    feat: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,39 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
 import { HeadProps, PageProps } from 'gatsby';
 import React from 'react';
 
+import { Button } from '../components';
+
 function NotFoundPage({}: PageProps) {
-  return <></>;
+  return (
+    <Box
+      alignItems="center"
+      display="flex"
+      flexDirection="column"
+      flexGrow={1}
+      gap={3}
+      justifyContent="center"
+      paddingX={3}
+    >
+      <Box alignItems="center" display="flex" flexDirection="column" justifyContent="center">
+        <Text fontSize="4xl" fontWeight={700}>
+          404
+        </Text>
+        <Heading as="h1" fontSize="lg" textAlign="center">
+          Page introuvable ou en construction
+        </Heading>
+      </Box>
+      <Button link colorScheme="primary" to="/">
+        Revenir à l'accueil
+      </Button>
+    </Box>
+  );
 }
 
 export default NotFoundPage;
 
 export function Head({}: HeadProps) {
-  return <title>Page introuvable</title>;
+  const title = 'Page introuvable | Vélotafons !';
+
+  return <title>{title}</title>;
 }
